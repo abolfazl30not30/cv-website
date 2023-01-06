40254f98ac32d0bbcee68b329809e75f6b553a38
Pedram <pedram.monazzami@gmail.com>
2023-01-06 15:57:10 +0330

    login fixed

--- src/component/admin/Admin.jsx
+++ src/component/admin/Admin.jsx
@@ -2,17 +2,19 @@ import React, {useState, useEffect} from 'react';
 import {Route, Routes} from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
-import Main from "./Main";
 import axios from "axios";
 
-let isAuth
-axios.get('http://localhost:8089/api/v1/admin/message', {headers: {'Authorization': localStorage.getItem('token')}}).then((response) => response.data)
-    .then(() => {
-        isAuth = true
-    }).catch(() => {
-    isAuth = false
-});
 function Admin() {
+
+    const [isAuth, setIsAuth] = useState(false)
+    useEffect(() => {
+        axios.get('http://localhost:8089/api/v1/admin/message', {headers: {'Authorization': localStorage.getItem('token')}}).then((response) => response.data)
+            .then(() => {
+                setIsAuth(true)
+            }).catch(() => {
+            setIsAuth(false)
+        });
+    });
     return (
         <div>
             <div className="d-flex justify-content-center align-items-center mainBody">
